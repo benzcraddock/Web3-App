7bce2965614934795299f79e9d853db52c665a8c
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-11 21:53:44 -0500

    built header and collection card template

---
+++ benyepunks/src/components/CollectionCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import weth from '../assets/weth.png';
+import './CollectionCard.css';
+
+const CollectionCard = ({id, name, traits, image}) => {
+  return (
+    <div className="collectionCard">
+      <img src={image} alt="" />
+      <div classNAme="details"></div>
+      <div className="name">
+        {name} <div className="id"> ･#{id}</div>
+      </div>
+      <div className="priceContainer">
+        <img src={weth} className="wethImage" alt="" />
+        <div className="price">
+          {traits[0]?.value}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CollectionCard;
